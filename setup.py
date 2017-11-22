f842de905be982aec55f728d0aa4beb0398c1ce7
markofu <markofu@users.noreply.github.com>
2017-11-21 16:45:46 -0800

    Correcting metadata for team name, lic & git url

--- setup.py
+++ setup.py
@@ -34,10 +34,10 @@ setuptools.setup(
     # Metadata for the project
     description='SQS Based Scheduler for Cloud Inquisitor',
     long_description=long_desc,
-    url='https://github.com/CloudInquisitor/cinq-scheduler-sqs/',
-    author='Riot Games InfoSec',
+    url='https://github.com/RiotGames/cinq-scheduler-sqs/',
+    author='Riot Games Security',
     author_email='security@riotgames.com',
-    license='MIT',
+    license='Apache 2.0',
     classifiers=[
         # Current project status
         'Development Status :: 4 - Beta',
@@ -47,7 +47,7 @@ setuptools.setup(
         'Intended Audience :: Information Technology',
 
         # License information
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: Apache 2.0',
 
         # Supported python versions
         'Programming Language :: Python :: 3.5',
